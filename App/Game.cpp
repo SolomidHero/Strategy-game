21ddeff77d30854e76145ed0b3625871fbb04e93
SolomidHero <dim.sad23@gmail.com>
2018-05-23 03:35:55 +0300

    game saving rationalized

--- App/Game.cpp
+++ App/Game.cpp
@@ -12,62 +12,75 @@ void Game::create() {
   app.set_builder(&Elfheim);
   cout << "Choose race or unit.\n 1. Elf\n 2. Healer\n 3. Warrior\n 4. Mage\n 5. Archmage\n";
 
-  // bool flag = true;
-  // while(flag) {
-  //   int cmd;
-  //   cin >> cmd;
-    // switch (cmd) {
-    //   case 1:
-    //     players[0]->add_unit(app.create_elf());
-    //     break;
-    //   case 2:
-    //     players[0]->add_unit(app.create_healer());
-    //     break;
-    //   case 3:
-    //     players[0]->add_unit(app.create_warrior());
-    //     break;
-    //   case 4:
-    //     players[0]->add_unit(app.create_mage());
-    //     break;
-    //   case 5:
-    //     players[0]->add_unit(app.create_archmage());
-    //     break;
-    //   default:
-    //     flag = false;
-    //     break;
-    // }
-  // }
+  bool flag = true;
+  while(flag) {
+    int cmd;
+    cin >> cmd;
+    std::unique_ptr<UnitInterface> unit;;
+    switch (cmd) {
+      case 1:
+        unit = app.create_elf();
+        players[0]->add_unit(unit);
+        break;
+      case 2:
+        unit = app.create_healer();
+        players[0]->add_unit(unit);
+        break;
+      case 3:
+        unit = app.create_warrior();
+        players[0]->add_unit(unit);
+        break;
+      case 4:
+        unit = app.create_mage();
+        players[0]->add_unit(unit);
+        break;
+      case 5:
+        unit = app.create_archmage();
+        players[0]->add_unit(unit);
+        break;
+      default:
+        flag = false;
+        break;
+    }
+    std::cout << "Unit created." << std::endl;
+  }
 
-  // std::unique_ptr<Player> Enemy (new Player());
-  // players.push_back(std::move(Enemy));
-  // int frag_num = rand() % EnemyMaxAmount;
+  std::unique_ptr<Player> Enemy (new Player());
+  players.push_back(std::move(Enemy));
+  int frag_num = rand() % EnemyMaxAmount;
 
-  // for (int i = 0; i < frag_num; ++i) {
-  //   int cmd = rand() % 5 + 1;
-  //   switch (cmd) {
-  //     case 1:
-  //       players[1]->add_unit(app.create_elf());
-  //       break;
-  //     case 2:
-  //       players[1]->add_unit(app.create_healer());
-  //       break;
-  //     case 3:
-  //       players[1]->add_unit(app.create_warrior());
-  //       break;
-  //     case 4:
-  //       players[1]->add_unit(app.create_mage());
-  //       break;
-  //     case 5:
-  //       players[1]->add_unit(app.create_archmage());
-  //       break;
-  //     default:
-  //       flag = false;
-  //       break;
-  //   }
-  // }
-  // saver = new Snapshot ();
-  // State save = { app, players };
-  // saver->add_save(&save);
+  for (int i = 0; i < frag_num; ++i) {
+    int cmd = rand() % 5 + 1;
+    std::unique_ptr<UnitInterface> unit;;
+    switch (cmd) {
+      case 1:
+        unit = app.create_elf();
+        players[1]->add_unit(unit);
+        break;
+      case 2:
+        unit = app.create_healer();
+        players[1]->add_unit(unit);
+        break;
+      case 3:
+        unit = app.create_warrior();
+        players[1]->add_unit(unit);
+        break;
+      case 4:
+        unit = app.create_mage();
+        players[1]->add_unit(unit);
+        break;
+      case 5:
+        unit = app.create_archmage();
+        players[1]->add_unit(unit);
+        break;
+      default:
+        flag = false;
+        break;
+    }
+  }
+  saver = new Snapshot ();
+  State save = { app, players };
+  saver->add_save(save);
 }
 
 void Game::save() {
